bf366e898bc03f04418c43182c2fdda7f2217970
fuji-184 <fujisantoso134@gmail.com>
2024-08-01 06:33:52 +0700

    updated

--- frontend/src/lib/Pengedit_Teks.svelte
+++ frontend/src/lib/Pengedit_Teks.svelte
@@ -1,3 +1,29 @@
 <script>
-	import { onMount } fro
+	import Editor from '@tinymce/tinymce-svelte'
+
+	let artikel = {
+		judul: "",
+		isi: ""
+	}
+
+	let conf = {
+	  height: 800,
+	  menubar: true,
+	  plugins: [
+		'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+		'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+		'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
+	  ],
+	  toolbar: 'undo redo | blocks | ' +
+		'bold italic forecolor | alignleft aligncenter ' +
+		'alignright alignjustify | bullist numlist outdent indent | ' +
+		'removeformat | help',
+	}
 </script>
+
+<div>
+	<input type="text" bind:value={artikel.judul} placeholder="judul" class="p-4 rounded-lg w-full focus:outline-none text-xl font-bold" />
+	<Editor scriptSrc='http://127.0.0.1:5173/tinymce/tinymce.min.js' bind:value={artikel.isi}
+  	{conf}
+	/>
+</div>
--- frontend/src/routes/admin/artikel/tambah/+page.svelte
+++ frontend/src/routes/admin/artikel/tambah/+page.svelte
@@ -1,33 +1,54 @@
 <script>
 	import Editor from '@tinymce/tinymce-svelte'
 
-	let artikel
+	let artikel = {
+		judul: "",
+		isi: ""
+	}
+	
+	const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const isSmallScreen = window.matchMedia('(max-width: 1023.5px)').matches;
 
 	let conf = {
-	  height: 500,
+	  height: 800,
 	  menubar: true,
+	  editimage_cors_hosts: ['picsum.photos'],
+	  autosave_ask_before_unload: true,
+	  autosave_interval: '30s',
+	  autosave_prefix: '{path}{query}-{id}-',
+	  autosave_restore_when_empty: false,
+	  autosave_retention: '2m',
+	  image_advtab: true,
+	  importcss_append: true,
+	  image_caption: true,
+	  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+	  noneditable_class: 'mceNonEditable',
+	  toolbar_mode: 'sliding',
+	  contextmenu: 'link image table',
+	  skin: useDarkMode ? 'oxide-dark' : 'oxide',
+	  //skin: 'borderless',
+	  //icons: 'thin',
+	  content_css: useDarkMode ? 'dark' : 'default',
+	  content_style: '* {background-color: black}',
 	  plugins: [
 		'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
 		'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-		'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
+		'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount', 'importcss', 'autosave', 'save', 'directionality', 'visualchars', 'codesample', 'pagebreak', 'nonbreaking', 'quickbars', 'emoticons', 'accordion'
 	  ],
-	  toolbar: 'undo redo | blocks | ' +
-		'bold italic forecolor | alignleft aligncenter ' +
-		'alignright alignjustify | bullist numlist outdent indent | ' +
+	  toolbar: 'undo redo | accordion accordionremove | blocks fontfamily fontsize | ' +
+		'bold italic underline strikethrough forecolor backcolor | alignleft aligncenter ' +
+		'alignright alignjustify | bullist numlist outdent indent lineheight | link image | table media | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | ltr rtl | ' +
 		'removeformat | help',
-
-		content_style: '* {background-color: black; color: white; border-radius: 16px}'
+		// license_key: 'gpl'
 	}
 </script>
 
 <div>
-  <Editor id="tes" scriptSrc='http://127.0.0.1:5173/tinymce/tinymce.min.js' bind:value={artikel}
-  	{conf}
+	<input type="text" bind:value={artikel.judul} placeholder="judul" class="p-4 rounded-lg w-full focus:outline-none text-xl font-bold" />
+	<Editor licenseKey='gpl' scriptSrc='http://127.0.0.1:5173/tinymce/tinymce.min.js'
+	
+		bind:value={artikel.isi}
+		{conf}
+		
 	/>
 </div>
-
-<style>
-	#tes {
-		background-color: black;
-	}
-</style>
